b262130a02801640b7edb26faf23e88ea78498c6
Vitalie Miron <mironvitaliev@gmail.com>
2023-04-10 18:23:24 +0300

    feat(home-banner): add home banner

---
+++ src/HomeBanner/index.tsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled"
+import { Container as MuiContainer, Typography, Button } from "@mui/material"
+import Link from "next/link"
+
+const Container = styled.div`
+  padding: 20px 0;
+  background: #e9dcd2;
+`
+
+export const HomeBanner = () => {
+  return (
+    <Container>
+      <MuiContainer>
+        <Typography variant="h2" component="h2" gutterBottom>
+          Obține consultanță juridică{" "}
+        </Typography>
+        <Typography variant="h5" component="h5" gutterBottom>
+          Întreabă un avocat gratuit și fără programare{" "}
+        </Typography>
+        <Link href="#chat">
+          <Button type="button" variant="outlined">
+            Adresează o intrebare
+          </Button>
+        </Link>
+      </MuiContainer>
+    </Container>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import Layout from "src/core/layouts/Layout"
 import { BlitzPage } from "@blitzjs/next"
+import { HomeBanner } from "src/HomeBanner"
 
 const Home: BlitzPage = () => {
-  return <div>test</div>
+  return (
+    <div>
+      <HomeBanner />
+    </div>
+  )
 }
 
 Home.getLayout = (page) => <Layout>{page}</Layout>
